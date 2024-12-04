b9566d931a7ef35723e18b445137aca02a9d845c
cosmicdawn42 <56638060+cosmicdawn42@users.noreply.github.com>
2024-12-04 09:53:43 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: /pos/index.php"); // Use the absolute path to the index page
+header("Location: index.php"); // Use the absolute path to the index page
 exit();
 ?>
