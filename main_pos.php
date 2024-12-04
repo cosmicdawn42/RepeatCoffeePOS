db077a6ab60c1c42b9183bc843f63f10cf4544d4
cosmicdawn42 <56638060+cosmicdawn42@users.noreply.github.com>
2024-12-04 09:54:04 +0800

    Update main_pos.php

--- main_pos.php
+++ main_pos.php
@@ -1,119 +1,65 @@
-<?php include 'header.php' ?>
-<?php include 'navbar.php' ?>
+<?php 
+include 'header.php';
+include 'navbar.php';
+?>
 
 <div class="pos-container">
     <div class="pos-grid" id="left-grid">
-        <table>
-            <tr>
-                <th>Qty</th>
-                <th>Description</th>
-                <th>Amount</th>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Cafe Latte (Iced) </td>
-                <td>150.00</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Americano (Hot) </td>
-                <td>65.00</td>
-            </tr>
+        <table id="order-table">
+            <thead>
+                <tr>
+                    <th>Qty</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody id="order-list">
+                <!-- Orders will dynamically populate here -->
+            </tbody>
         </table>
         <div>
-            
+            <p>Total: <span id="total-amount">0.00</span></p>
         </div>
     </div>
     <div class="pos-grid" id="right-grid">
         <div class="menu-grid">
-            <div class="menu-item">
-                <button>Americano</button>
-            </div>
-            <div class="menu-item">
-                <button>Almond Latte</button>
-            </div>
-            <div class="menu-item">
-                <button>Cafe Latte</button>
-            </div>
-            <div class="menu-item">
-                <button>Caramel Latte</button>
-            </div>
-            <div class="menu-item">
-                <button>Cinnamon</button>
-            </div>
-            <div class="menu-item">
-                <button>Dark Mocha</button>
-            </div>
-            <div class="menu-item">
-                <button>English Toffee</button>
-            </div>
-            <div class="menu-item">
-                <button>Hazelnut Latte</button>
-            </div>
-            <div class="menu-item">
-                <button>Spanish Latte</button>
-            </div>
-            <div class="menu-item">
-                <button>Classic Chocolate</button>
-            </div>
-            <div class="menu-item">
-                <button>Milky Caramel</button>
-            </div>
-            <div class="menu-item">
-                <button>Matcha</button>
-            </div>
+            <!-- Menu Buttons -->
+            <?php
+            $menu_items = [
+                "Americano" => 65.00, "Almond Latte" => 150.00,
+                "Cafe Latte" => 150.00, "Caramel Latte" => 160.00,
+                "Cinnamon" => 70.00, "Dark Mocha" => 180.00,
+                "English Toffee" => 170.00, "Hazelnut Latte" => 155.00,
+                "Spanish Latte" => 165.00, "Classic Chocolate" => 150.00,
+                "Milky Caramel" => 145.00, "Matcha" => 140.00
+            ];
+            foreach ($menu_items as $item => $price) {
+                echo "<div class='menu-item'><button onclick=\"addItem('$item', $price)\">$item</button></div>";
+            }
+            ?>
         </div>
         <div class="add-ons">
             <div class="menu-grid">
-                <div class="menu-item">
-                    <button>Caramel</button>
-                </div>
-                <div class="menu-item">
-                    <button>Espresso Shot</button>
-                </div>
-                <div class="menu-item">
-                    <button>Hazelnut</button>
-                </div>
-                <div class="menu-item">
-                    <button>Vanilla</button>
-                </div>
-                <div class="menu-item">
-                    <button></button>
-                </div>
-                <div class="menu-item">
-                    <button></button>
-                </div>
+                <div class="menu-item"><button onclick="addItem('Caramel', 20.00)">Caramel</button></div>
+                <div class="menu-item"><button onclick="addItem('Espresso Shot', 30.00)">Espresso Shot</button></div>
+                <div class="menu-item"><button onclick="addItem('Hazelnut', 25.00)">Hazelnut</button></div>
+                <div class="menu-item"><button onclick="addItem('Vanilla', 20.00)">Vanilla</button></div>
             </div>
         </div>
         <div class="primarybtns">
             <div class="menu-grid">
-                <div class="menu-item">
-                    <button style="background-color: #C98F4F">Hot</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #C98F4F">Iced</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #65ceff">Add</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #bb0d12">Remove</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #FA7570">Clear Items</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #CCD6D6"><</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #ffe168">Print Receipt</button>
-                </div>
-                <div class="menu-item">
-                    <button style="background-color: #2E981E">Enter</button>
-                </div>
+                <div class="menu-item"><button style="background-color: #C98F4F" onclick="toggleOption('Hot')">Hot</button></div>
+                <div class="menu-item"><button style="background-color: #C98F4F" onclick="toggleOption('Iced')">Iced</button></div>
+                <div class="menu-item"><button style="background-color: #65ceff" onclick="addSelected()">Add</button></div>
+                <div class="menu-item"><button style="background-color: #bb0d12" onclick="removeSelected()">Remove</button></div>
+                <div class="menu-item"><button style="background-color: #FA7570" onclick="clearItems()">Clear Items</button></div>
+                <div class="menu-item"><button style="background-color: #ffe168" onclick="printReceipt()">Print Receipt</button></div>
             </div>
         </div>
     </div>
 </div>
 
-<?php include 'footer.php' ?>
+<!-- Include external JavaScript -->
+<script src="mainposscript.js"></script>
+
+<?php include 'footer.php'; ?>
